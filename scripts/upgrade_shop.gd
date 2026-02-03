1bb23ba5924de232189e2d001091cb777eb65bab
SearedBacon <2260910@flourbluffschools.org>
2026-02-03 12:31:34 -0600

    added a new level with a boat asset and started making an upgrade menu

---
+++ scripts/upgrade_shop.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _on_depth_buton_pressed() -> void:
+	pass
+
+func _on_reel_speed_button_pressed() -> void:
+	pass
+
+func _on_line_speed_button_pressed() -> void:
+	pass
