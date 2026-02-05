b7f46a7b1b7e5b53582ae25ab50de08ab8d93298
SearedBacon <2260910@flourbluffschools.org>
2026-02-05 12:40:11 -0600

    Added another upgrade to the shop and added a close button. Created player and added more to the upgrade shop house.

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,1 +1,7 @@
 extends Node3D
+
+@onready var upgrade_shop: CanvasLayer = $UpgradeShop
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player:
+		upgrade_shop.visible=true
--- scripts/upgrade_shop.gd
+++ scripts/upgrade_shop.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
+@onready var upgrade_shop: CanvasLayer = $"."
+
 func _on_depth_buton_pressed() -> void:
 	pass
 
 func _on_reel_speed_button_pressed() -> void:
 	pass
 
-func _on_line_speed_button_pressed() -> void:
+func _on_line_strength_button_pressed() -> void:
+	pass 
+
+func _on_luck_button_pressed() -> void:
 	pass
+
+func _on_close_button_pressed() -> void:
+	upgrade_shop.visible=false
