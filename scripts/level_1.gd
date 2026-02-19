9f16d53da83bc512664bf2ef65a791fa5e63b10f
SearedBacon <2260910@flourbluffschools.org>
2026-02-19 12:39:08 -0600

    Started importing more fish assets

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var shop: CanvasLayer = $Shop
 @onready var clown_fih: Node3D = $ClownFih
+@onready var kingfish: Node3D = $Kingfish
+@onready var goldfish: Node3D = $Goldfish
 
 func _process(delta: float) -> void:
 	if Globals.fish_caught==1:
@@ -9,6 +11,8 @@ func _process(delta: float) -> void:
 	else:
 		clown_fih.visible=false
 	clown_fih.rotate_y(.025)
+	kingfish.rotate_y(.025)
+	goldfish.rotate_y(.025)
 	
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
